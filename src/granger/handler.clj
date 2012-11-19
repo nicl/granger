268f255f405f6a010abb7735c8f1f5926cf5141b
Nicolas Long <nicolaslong@gmail.com>
2012-11-19 16:37:09 +0000

    added basic regex check on find by isbn endpoint

--- src/granger/handler.clj
+++ src/granger/handler.clj
@@ -7,7 +7,7 @@
 (defroutes app-routes
   (GET "/" [] "Welcome to my book reviews site!")
   (GET "/books" [] (books/find-books))
-  (GET "/books/:isbn" [isbn] (books/find-book isbn))
+  (GET ["/books/:isbn", :isbn #"[-0-9]*"] [isbn] (books/find-book isbn))
   (POST "/books" [body] (books/add-book! body))
   (PUT "/books/:isbn" [isbn] (books/update-book! isbn))
   (route/not-found "Not Found"))
