2d14cc72b3b6871ac3587dfde47052395e84e375
Nicolas Long <nicolaslong@gmail.com>
2012-11-22 15:45:06 +0000

    added skeleton tests

---
+++ test/granger/test/models/books.clj
@@ -0,0 +1,18 @@
+(ns granger.test.models.books
+  (:use clojure.test
+        granger.models.books))
+
+(deftest add-test
+  (is true))
+
+(deftest update-test
+  (is true))
+
+(deftest find-test
+  (is true))
+
+(deftest find-all-test
+  (is true))
+
+(deftest is-valid-test
+  (is true))
